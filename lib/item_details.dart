89570246192e9fe02eef43194f0ca3b4d1b7b216
Himanshu <himanshupednekar036@gmail.com>
2019-08-12 16:52:36 +0530

    order button added

--- lib/item_details.dart
+++ lib/item_details.dart
@@ -5,11 +5,10 @@ class ItemDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Chicken Tikka Masala")),
-      body: Column(
-        children: <Widget>[
-          ItemImage(),
-          ItemDescription(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[ItemImage(), ItemDescription(), AddItemButton()],
+        ),
       ),
     );
   }
@@ -33,12 +32,12 @@ class ItemDescription extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.centerLeft,
-      margin: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+      margin: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 25.0),
       child: Text(
         "Chicken tikka masala is composed of chicken tikka, boneless chunks of chicken marinated in spices and yogurt that are roasted in an oven, served in a creamy curry sauce.",
         style: TextStyle(
-            fontSize: 23.0,
-            fontFamily: "Ubuntu",
+            fontSize: 24.0,
+            fontFamily: "Roboto",
             fontWeight: FontWeight.w400,
             letterSpacing: 1.1),
       ),
@@ -46,18 +45,40 @@ class ItemDescription extends StatelessWidget {
   }
 }
 
-/*
-Align(
-        //   alignment: Alignment.centerLeft,
-        //   child: Text(
-        //     "Description",
-        //     textAlign: TextAlign.start,
-        //     textDirection: TextDirection.ltr,
-        //     style: TextStyle(
-        //         fontSize: 40.0,
-        //         fontFamily: 'Ubuntu',
-        //         fontWeight: FontWeight.w700,
-        //         color: Color.fromRGBO(56, 56, 56, 1),
-        //         letterSpacing: 1.5),
-        //   ),
-        // ),*/
+class AddItemButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+      width: 300.0,
+      height: 50.0,
+      child: RaisedButton(
+        color: Color.fromRGBO(0, 179, 0, 1),
+        child: Text(
+          "ORDER",
+          style: TextStyle(
+              fontSize: 30.0,
+              color: Color.fromRGBO(16, 16, 16, 1),
+              fontFamily: 'Ubuntu',
+              fontWeight: FontWeight.w700,
+              letterSpacing: 3.0),
+        ),
+        elevation: 7,
+        onPressed: () {
+          showSnackBar(context);
+        },
+      ),
+    );
+  }
+}
+
+void showSnackBar(BuildContext context) {
+  var snackBar = SnackBar(
+    content: Text(
+      "Added To Order",
+      style: TextStyle(fontSize: 20.0),
+    ),
+  );
+
+  Scaffold.of(context).showSnackBar(snackBar);
+}
