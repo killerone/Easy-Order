f80c75edc33b5dc8c7cc9d6156f0a96cac094247
Himanshu <himanshupednekar036@gmail.com>
2019-08-25 13:23:13 +0530

    cart is 80% done

--- lib/cart.dart
+++ lib/cart.dart
@@ -5,59 +5,94 @@ class Cart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Stack(
-                      children: <Widget>[
-                        Container(
-                          height: 150.0,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image:
-                                      AssetImage('asset/images/green_bg.png'),
-                                  fit: BoxFit.cover)),
-                        ),
-                        Padding(
-                            padding: EdgeInsets.only(right: 15.0),
-                            child: IconButton(
-                              icon: Icon(Icons.arrow_back),
-                              color: Colors.black,
-                              onPressed: () {},
-                            )),
-                        Positioned(
-                          top: 65.0,
-                          left: 20.0,
-                          child: Text(
-                            "My Cart",
-                            style: TextStyle(
-                                fontFamily: 'Montserrat',
-                                fontSize: 30.0,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Positioned(
-                          top: 250.0,
-                          child: Column(
-                            children: <Widget>[
-                              itemCard('Finn Navian-Sofa', '248',
-                                  'asset/images/green_bg.png', 2, 0, context),
-                            ],
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                )
-              ],
-            )
-          ],
+          child: Column(
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Container(
+                    height: 150.0,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('asset/images/green_bg.png'),
+                            fit: BoxFit.cover)),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(right: 15.0),
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_back),
+                        color: Colors.black,
+                        onPressed: () {},
+                      )),
+                  Positioned(
+                    top: 65.0,
+                    left: 20.0,
+                    child: Text(
+                      "My Cart",
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 30.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  itemCard('Chicken tikka masala', '248',
+                      'asset/images/chickentikka.jpg', 2, 0, context),
+                  itemCard('Chicken tikka masala', '248',
+                      'asset/images/chickentikka.jpg', 2, 0, context),
+                  itemCard('Chicken tikka masala', '248',
+                      'asset/images/ChickenTikkaMasala.jpg', 2, 0, context),
+                  itemCard('Chicken tikka masala', '248',
+                      'asset/images/chickentikka.jpg', 2, 0, context),
+                ],
+              ),
+            ],
+          )
+        ],
+      )),
+      bottomNavigationBar: Material(
+        color: Colors.grey.withOpacity(0.2),
+        child: Container(
+          height: 50.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width / 2,
+                child: Text(
+                  'Total : \₹ 1000',
+                  style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width / 2,
+                child: RaisedButton(
+                  elevation: 0.5,
+                  onPressed: () {},
+                  color: Colors.orange.withOpacity(0.7),
+                  child: Text(
+                    "Place Order",
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 20.0,
+                        color: Colors.white,
+                        letterSpacing: 1.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -74,61 +109,70 @@ class Cart extends StatelessWidget {
               child: Container(
                 padding: EdgeInsets.only(left: 15.0, right: 15.0),
                 width: MediaQuery.of(context).size.width - 20,
-                height: 250,
+                height: 120,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Row(
                   children: <Widget>[
+                    SizedBox(width: 5.0),
+                    Container(
+                      height: 150.0,
+                      width: 125.0,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              // Item image
+                              image: AssetImage(imagePath),
+                              fit: BoxFit.contain)),
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
                     Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Container(
-                          height: 25.0,
-                          width: 25.0,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12.5)),
-                        ),
-                        SizedBox(width: 10.0),
-                        Container(
-                          height: 150.0,
-                          width: 125.0,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: AssetImage(imagePath),
-                                  fit: BoxFit.contain)),
-                        ),
-                        SizedBox(
-                          width: 4.0,
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        Row(
                           children: <Widget>[
-                            Row(
-                              children: <Widget>[
-                                Text(
-                                  itemName,
-                                  style: TextStyle(
-                                      fontFamily: 'Montserrat',
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 15.0),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 7.0,
-                            ),
                             Text(
-                              '\₹ 350',
+                              // Item name
+                              itemName,
                               style: TextStyle(
                                   fontFamily: 'Montserrat',
-                                  fontSize: 25.0,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17.0),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 7.0,
+                        ),
+                        // Item price
+                        Row(
+                          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            Text(
+                              '\₹ $price',
+                              style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 15.0,
                                   color: Colors.black,
                                   fontWeight: FontWeight.bold),
                             ),
+                            // Row(
+                            //   // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            //   children: <Widget>[
+                            //     Container(
+                            //       decoration: BoxDecoration(
+                            //           color: Colors.grey.withOpacity(0.4)),
+                            //       child: Text("+"),
+                            //     )
+                            //   ],
+                            // )
                           ],
-                        )
+                        ),
                       ],
                     )
                   ],
