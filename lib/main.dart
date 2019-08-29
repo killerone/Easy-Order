3ccd29bab26cad69fa4ad32ca76f0cb95d8604b2
Anuradha <anudate97@gmail.com>
2019-08-29 13:18:20 +0530

    Anuradha

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(Text("Hello Flutter",textDirection: TextDirection.ltr));
+  runApp(
+    Center(
+      child:
+        Text("Hello Flutter",textDirection: TextDirection.ltr)
+    ),
+  );
+
 }
